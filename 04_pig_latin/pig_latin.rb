a0e77cff3e1dd556da959f087879f618eb87c914
Christopher Yeung <christopheryeung1@gmail.com>
2018-10-25 16:44:48 -0400

    Complete exercise

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
 #write your code here
+def translate str
+    vowel = %w[a e i o u]
+    alphabet = ('a'..'z').to_a
+    consonant = alphabet - vowel
+
+    str.split(' ').map do |w|
+        if vowel.include?(w[0])
+            w + 'ay'
+        elsif w.include?('qu')
+            if ('qu').include?(w[0..1])
+                w[2..-1] + w[0..1] + 'ay'
+            elsif consonant.include?(w[0]) && ('qu').include?(w[1..2])
+                w[3..-1] + w[0..2] + 'ay'
+            end
+        elsif consonant.include?(w[0]) && consonant.include?(w[1]) && consonant.include?(w[2])
+            w[3..-1] + w[0..2] + 'ay'
+        elsif consonant.include?(w[0]) && consonant.include?(w[1])
+            w[2..-1] + w[0..1] + 'ay'
+        elsif consonant.include?(w[0])
+            w[1..-1] + w[0] + 'ay'
+        else
+            w
+        end
+    end.join(' ')
+end

7af92c305423959f682caf0d36b3bd05f4583967
Christopher Yeung <christopheryeung1@gmail.com>
2018-10-24 22:05:57 -0400

    Complete exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add a, b
+    a + b
+end
+
+def subtract a, b
+    a - b
+end
+
+def sum numbers
+    numbers.inject(0) {|sum, i| sum + i}
+end
+
+def multiply numbers
+    numbers.inject(1) {|product, i| product * i}
+end
+
+def power a, b
+    a ** b
+end
+
+def factorial a
+    (1..a).inject(:*) || 1
+end

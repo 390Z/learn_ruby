a200d87c38138ac8fce689f0a4792c525a990053
Christopher Yeung <christopheryeung1@gmail.com>
2018-10-26 11:19:51 -0400

    rake

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
   #write your code here
+  attr_writer :seconds, :minutes
+
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    minutes = @seconds / 60
+    hours = minutes / 60
+    format("%02d:%02d:%02d", hours, minutes%60, seconds%60)
+  end
 end

7b561b34f1f140a438a5f5d6a1e6aabb374f7ecf
Christopher Yeung <christopheryeung1@gmail.com>
2018-10-26 10:59:46 -0400

    Complete exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
 # write your code here
+    attr_writer :title
+
+    def title
+        @conjuctions = %w[and that but or as if than because while]
+        @prepositions = %w[of in to for with on at from by about]
+        @articles = %w[the a an]
+
+        @title.split.map.with_index do |w, i|
+            stop_words = @conjuctions + @prepositions + @articles
+            unless (stop_words.include? w) and (i > 0)
+                w.capitalize
+            else
+                w
+            end
+        end.join(' ')
+    end
 end
